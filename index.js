0969b50b5c5c5b7021f9c98f0985ee2ffeca1de2
redassser <40395425+redassser@users.noreply.github.com>
2018-10-11 19:43:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -62,37 +62,40 @@ if (command === "ping") {
         } else {
           
           let ser1 = json.find(o => o.ip === "149.202.87.101" && o.port === '7790')
-          if(!ser1) {var title1 = "[OFFLINE]"; var player1 = "N/A"} 
-          else {var title1 = ""; var player1 = ser1.players}
+          if(!ser1) {var title1 = "[OFFLINE]"; var player1 = "N/A"; var color1 = "#1de535"} 
+          else {var title1 = ""; var player1 = ser1.players; var color1 = "#e51c1c"}
            
           let ser2 = json.find(o => o.ip === "149.202.87.101" && o.port === '7778')
-          if(!ser2) {var title2 = "[OFFLINE]"; var player2 = "N/A"} 
-          else {var title2 = ""; var player2 = ser2.players}
+          if(!ser2) {var title2 = "[OFFLINE]"; var player2 = "N/A"; var color2 = "#1de535"} 
+          else {var title2 = ""; var player2 = ser2.players; var color2 = "#e51c1c"}
            
           let ser3 = json.find(o => o.ip === "149.202.87.101" && o.port === '7779')
-          if(!ser3) {var title3 = "[OFFLINE]"; var player3 = "N/A"} 
-          else {var title3 = ""; var player3 = ser3.players}
+          if(!ser3) {var title3 = "[OFFLINE]"; var player3 = "N/A"; var color3 = "#1de535"} 
+          else {var title3 = ""; var player3 = ser3.players; var color3 = "#e51c1c"}
 		
 	  let serverstatus1 = new Discord.RichEmbed()
 	    .setTitle("Shiba community #1 "+title1)
 	    .setAuthor("SCP Secret Laboratory","http://scp-sl.wdfiles.com/local--files/nav:side/scp-sl-logo.png")
-	    .addField("IP:","scpmemers.xyz")
-	    .addField("PORT:","7790")
-	    .addField("PLAYERS:",player1)
+	    .addField("IP:","scpmemers.xyz",true)
+	    .addField("PORT:","7790",true)
+	    .addField("PLAYERS:",player1,true)
+	    .setColor(color1)
           message.channel.send(serverstatus1)
 	  let serverstatus2 = new Discord.RichEmbed()
 	    .setTitle("Shiba community #2 "+title2)
 	    .setAuthor("SCP Secret Laboratory","http://scp-sl.wdfiles.com/local--files/nav:side/scp-sl-logo.png")
-	    .addField("IP:","scpmemers.xyz")
-	    .addField("PORT:","7778")
-	    .addField("PLAYERS:",player2)
+	    .addField("IP:","scpmemers.xyz",true)
+	    .addField("PORT:","7778",true)
+	    .addField("PLAYERS:",player2,true)
+	    .setColor(color2)
           message.channel.send(serverstatus2)
 	  let serverstatus3 = new Discord.RichEmbed()
 	    .setTitle("Shiba community #3 "+title3)
 	    .setAuthor("SCP Secret Laboratory","http://scp-sl.wdfiles.com/local--files/nav:side/scp-sl-logo.png")
-	    .addField("IP:","scpmemers.xyz")
-	    .addField("PORT:","7779")
-	    .addField("PLAYERS:",player3)
+	    .addField("IP:","scpmemers.xyz",true)
+	    .addField("PORT:","7779",true)
+	    .addField("PLAYERS:",player3,true)
+	    .setColor(color3)
           message.channel.send(serverstatus3)
         } 
       }  
