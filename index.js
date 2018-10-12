e75013c83ef1968b762c72f159e0f6d9e4827606
redassser <40395425+redassser@users.noreply.github.com>
2018-10-12 11:45:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -60,24 +60,25 @@ if (command === "set") {
         if ("error" in json) {
           console.log("Someone help me!");
         } else {
+          var serv1 = client.sets.get("server1"); var serv2 = client.sets.get("server2"); var serv3 = client.sets.get("server3")
           
-          let ser1 = json.find(o => o.ip === client.sets.get("server1")[0] && o.port === client.sets.get("server1")[1])
+          let ser1 = json.find(o => o.ip === serv1[0] && o.port === serv1[1])
           if(!ser1) {var title1 = "[OFFLINE]"; var player1 = "N/A"; var color1 = "#e51c1c"} 
           else {var title1 = ""; var player1 = ser1.players; var color1 = "#1de535"}
            
-          let ser2 = json.find(o => o.ip === client.sets.get("server2")[0] && o.port === client.sets.get("server2")[1])
+          let ser2 = json.find(o => o.ip === serv2[0] && o.port === serv2[1])
           if(!ser2) {var title2 = "[OFFLINE]"; var player2 = "N/A"; var color2 = "#e51c1c"} 
           else {var title2 = ""; var player2 = ser2.players; var color2 = "#1de535"}
            
-          let ser3 = json.find(o => o.ip === client.sets.get("server3")[0] && o.port === client.sets.get("server3")[1])
+          let ser3 = json.find(o => o.ip === serv3[0] && o.port === serv3[1])
           if(!ser3) {var title3 = "[OFFLINE]"; var player3 = "N/A"; var color3 = "#e51c1c"} 
           else {var title3 = ""; var player3 = ser3.players; var color3 = "#1de535"}
 		
 	  let serverstatus1 = new Discord.RichEmbed()
 	    .setTitle("Shiba community #1 "+title1)
 	    .setAuthor("SCP Secret Laboratory","https://upload.wikimedia.org/wikipedia/commons/e/ec/SCP_Foundation_%28emblem%29.svg")
-	    .addField("IP:","scpmemers.xyz",true)
-	    .addField("PORT:","7790",true)
+	    .addField("IP:",serv1[0],true)
+	    .addField("PORT:",serv1[1],true)
 	    .addField("PLAYERS:",player1,true)
 	    .setColor(color1)
           message.channel.send(serverstatus1)
