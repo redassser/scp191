0e6fb67c92fbc475e5ac1aca69492034d6015bef
redassser <40395425+redassser@users.noreply.github.com>
2018-10-12 08:52:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var Discord = require("discord.js");
+var Enmap = require('enmap');
+var EnmapMongo = require("enmap-mongo");
 var client = new Discord.Client();
 var prefix = "[]";
 var request = require('request');
 var cheerio = require('cheerio');
-var fs = require("fs");
+set.commands = new Enmap({ provider: new EnmapMongo({
+  name: `commands`,
+  dbName: `shibaset`,
+  url: process.env.MONGO
+})
+})
 client.on("ready", () => {
-  console.log("I am ready!");
+  console.log("Prepared for war");
   client.user.setActivity("[]ss");
 });
 
@@ -13,46 +20,23 @@ client.on("message", (message) => {
   var args = message.content.slice(prefix.length).trim().split(/ +/g);
   var command = args.shift().toLowerCase();
   var argu = args.join(" ");
-  var data = 0;
-  var ip = fs.readFileSync('./ip.json',"utf8" );
-  var port1 = fs.readFileSync('./port1.json',"utf8" );
-  var port2 = fs.readFileSync('./port2.json',"utf8" );
-  var port3 = fs.readFileSync('./port3.json',"utf8" );
+if (!message.content.startsWith(prefix)) return
 if (command === "ping") {
-   if (message.member.roles.has("473398252889178113")) {
-      message.member.removeRole("473398252889178113");
-      message.channel.send(`${message.author} will no longer be pinged for announcements.`);
-      message.delete();
-    } else {
-      message.member.addRole("473398252889178113");
-      message.channel.send(`${message.author} will now be pinged for announcements!`);
-      message.delete();
+  if (message.member.roles.has("473398252889178113")) {
+    message.member.removeRole("473398252889178113");
+    message.channel.send(message.author+` will no longer be pinged for announcements.`);
+    message.delete();
+  } else {
+    message.member.addRole("473398252889178113");
+    message.channel.send(message.author+` will now be pinged for announcements!`);
+    message.delete();
+  }
 }
+if (command === "set") {
+  if (!message.member.permissions.has('ADMINISTRATOR')) {message.channel.send("Admin only!");return}
+  if (args.length != 2) {message.channel.send("``[]set [1,2, or 3] [ip] [port]``");return}
+  
 }
-  if (command === "set" && args[0] != "" && message.member.permissions.has('ADMINISTRATOR')) {
-    fs.writeFile('./ip.json', `${args[0]}`, (err) => {
-  if (err) throw err;
-  console.log('The file has been saved! ');
-});
-  }
-  if (command === "set1" && args[0] != "" && message.member.permissions.has('ADMINISTRATOR')) {
-    fs.writeFile('./port1.json', `${args[0]}`, (err) => {
-  if (err) throw err;
-  console.log('The port1 has been saved!');
-});
-  }
-  if (command === "set2" && args[0] != "" && message.member.permissions.has('ADMINISTRATOR')) {
-    fs.writeFile('./port2.json', `${args[0]}`, (err) => {
-  if (err) throw err;
-  console.log('The port2 has been saved!');
-});
-  }
-  if (command === "set3" && args[0] != "" && message.member.permissions.has('ADMINISTRATOR')) {
-    fs.writeFile('./port3.json', `${args[0]}`, (err) => {
-  if (err) throw err;
-  console.log('The port3 has been saved!');
-});
-  }
   if (command === "ss"){
   request('https://api.scpslgame.com/lobbylist.php?format=json', function(err, resp, html) {
       if (!err){
