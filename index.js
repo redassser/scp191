7425cd875d5e318b8ab36bae1a057b33d7ef2525
redassser <40395425+redassser@users.noreply.github.com>
2018-10-12 10:05:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -35,14 +35,14 @@ if (command === "ping") {
 if (command === "set") {
   if (!message.member.permissions.has('ADMINISTRATOR')) {message.channel.send("Admin only!");return}
   if (args.length != 3) {message.channel.send("``[]set [1,2, or 3] [ip] [port]``");return}
-  switch (args[0]) {
-    case 1:
+  switch (args[0].trim()) {
+    case "1":
       client.sets.set("server1", [ args[1] , args[2] ])
       break;
-    case 2:
+    case "2":
       client.sets.set("server2", [ args[1] , args[2] ])
       break;
-    case 3:
+    case "3":
       client.sets.set("server3", [ args[1] , args[2] ]) 
       break;
     default:
